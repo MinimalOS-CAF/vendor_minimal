63ad3f4dc8c8de1e360d1d23f8889a121e993676
Griffin Millender <griffinn.millender@gmail.com>
2016-01-14 16:09:08 -0600

    Cleanup bootanimation

---
+++ prebuilt/bootanimation/Android.mk
@@ -0,0 +1,44 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+
+# determine the smaller dimension
+TARGET_BOOTANIMATION_SIZE := $(shell \
+  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
+    echo $(TARGET_SCREEN_WIDTH); \
+  else \
+    echo $(TARGET_SCREEN_HEIGHT); \
+  fi )
+
+# get a sorted list of the sizes
+bootanimation_sizes := $(subst .zip,, $(shell ls $(LOCAL_PATH)))
+bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
+
+# find the appropriate size and set
+define check_and_set_bootanimation
+$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
+    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
+      echo $(1); \
+      exit 0; \
+    fi;
+  fi;
+  echo $(TARGET_BOOTANIMATION_NAME); ))
+endef
+$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
+
+ifeq ($(TARGET_BOOTANIMATION_HALF_RES),true)
+bootanimation_file := halfres/$(TARGET_BOOTANIMATION_NAME).zip
+else
+bootanimation_file := $(TARGET_BOOTANIMATION_NAME).zip
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bootanimation.zip
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := MEDIA
+LOCAL_SRC_FILES := $(bootanimation_file)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/media
+include $(BUILD_PREBUILT)
+
+endif
